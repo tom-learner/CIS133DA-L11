54f8d612c59da14f864c3201c1e3f132e79b0566
tom-learner <liweiqitom@163.com>
2026-01-11 13:19:15 +0800

    L9-mission

---
+++ scripts/javascript.js
@@ -0,0 +1,25 @@
+/*
+Author: Li Weiqi
+Course: CIS133DA
+Section: #31862
+Lesson: 11
+*/
+
+// Display last modified date on the page
+const lastModifiedElement = document.getElementById("lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = document.lastModified;
+}
+
+// Show alert when form is submitted
+function formSubmit() {
+    alert("Form has been submitted successfully!");
+}
+
+// Show reset message when form is reset
+function formReset() {
+    const resetMessage = document.getElementById("resetMessage");
+    if (resetMessage) {
+        resetMessage.style.visibility = "visible";
+    }
+}
